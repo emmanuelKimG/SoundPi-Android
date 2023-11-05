0d5ec25a096e3ae6f27764e5535945a20c5a1c8a
Emmanuel Kim <carvente.e@mexrentacar.com>
2023-11-05 14:00:14 -0600

    First Commit

---
+++ app/src/main/java/com/example/soundpi/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.soundpi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
