e2e41352d2930c85bf293d0fa2999aa5c454e8c6
Emmanuel Kim <carvente.e@mexrentacar.com>
2023-11-20 20:30:50 -0600

    Lista de Dispositivos Agregada en back

--- app/src/main/java/com/example/soundpi/MainActivity.kt
+++ app/src/main/java/com/example/soundpi/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.soundpi
 
 import android.Manifest.permission.*
-import android.app.Activity
+import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
@@ -24,85 +24,71 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var bluetoothAdapter : BluetoothAdapter
+
+    @SuppressLint("MissingPermission")
     private var requestBluetooth = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         if (result.resultCode == RESULT_OK) {
-
+            bluetoothAdapter.startDiscovery()
         }else{
             Toast.makeText(this, "Device has been connected", Toast.LENGTH_SHORT).show()
         }
     }
 
-    private lateinit var searchList : ArrayList<BluetoothDevice>
-
     private val receiver = object : BroadcastReceiver() {
 
+        @SuppressLint("MissingPermission")
         override fun onReceive(context: Context, intent: Intent) {
-            val action: String? = intent.action
-            when(action) {
+            when(intent.action) {
                 BluetoothDevice.ACTION_FOUND -> {
-                    checkPermission()
                     val device: BluetoothDevice? =
                         intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-                    val deviceName = device?.name
-                    val deviceHardwareAddress = device?.address // MAC address
+
+                    Log.i("APP_STATUS","DEVICE FOUND : ${device!!.name}")
                 }
             }
         }
     }
 
+
+    @SuppressLint("MissingPermission")
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
         installSplashScreen()
-        checkPermission()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            checkPermission()
+        }
         setContentView(R.layout.activity_main)
 
         val searchBluetoothBtn = findViewById<Button>(R.id.search_bluetooth_device_btn)
         val bluetoothManager =  getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-        val bluetoothAdapter : BluetoothAdapter? = bluetoothManager.adapter
-        val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
-
-
-        Log.i("CONNECTED DEVICES",pairedDevices.toString())
-
-        if(bluetoothAdapter == null){
-            Toast.makeText(this, "This Device does not have Bluetooth", Toast.LENGTH_SHORT).show()
-        }
-
-        searchBluetoothBtn?.setOnClickListener {
-            if (bluetoothAdapter != null && !bluetoothAdapter.isEnabled) {
-                val bluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                requestBluetooth.launch(bluetoothIntent)
-            }
-        }
-
+        bluetoothAdapter = bluetoothManager.adapter
+        val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter.bondedDevices
         val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
         registerReceiver(receiver, filter)
 
-        super.onCreate(savedInstanceState)
-    }
-
-
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1) {
-            when (resultCode) {
-                Activity.RESULT_OK -> {
-                    Toast.makeText(this, "Esta OK", Toast.LENGTH_SHORT).show()
-                }
-                Activity.RESULT_CANCELED -> {
-                    Toast.makeText(this, "No esta OK ", Toast.LENGTH_SHORT).show()
-                }
+        searchBluetoothBtn?.setOnClickListener {
+            if (!bluetoothAdapter.isEnabled) {
+                val bluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                requestBluetooth.launch(bluetoothIntent)
+            }
+            else
+            {
+                bluetoothAdapter.startDiscovery()
             }
         }
+
     }
 
+    @RequiresApi(Build.VERSION_CODES.S)
     private fun checkPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             if (ContextCompat.checkSelfPermission(baseContext, ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(
                     this,
-                    arrayOf(ACCESS_BACKGROUND_LOCATION),
+                    arrayOf(ACCESS_BACKGROUND_LOCATION, ACCESS_COARSE_LOCATION),
                     100 )
             }
             if (ContextCompat.checkSelfPermission(baseContext, BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
@@ -110,6 +96,18 @@ class MainActivity : AppCompatActivity() {
                     this,
                     arrayOf(BLUETOOTH_CONNECT),
                     101 )
+            }
+            if (ContextCompat.checkSelfPermission(baseContext, BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(BLUETOOTH_SCAN),
+                    102 )
+            }
+            if (ContextCompat.checkSelfPermission(baseContext, BLUETOOTH_ADMIN) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(BLUETOOTH_ADMIN),
+                    103 )
             }
         }
     }
